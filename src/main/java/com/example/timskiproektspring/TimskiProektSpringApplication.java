350a12856b4a0df7a7dece0042e033ea67455c53
Gadzata <andrijan-kata@hotmail.com>
2022-12-26 14:34:04 +0100

    Initial commit

---
+++ src/main/java/com/example/timskiproektspring/TimskiProektSpringApplication.java
@@ -0,0 +1,13 @@
+package com.example.timskiproektspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TimskiProektSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TimskiProektSpringApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/timskiproektspring/TimskiProektSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.timskiproektspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TimskiProektSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
